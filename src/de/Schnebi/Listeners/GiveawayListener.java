20934d0c1efeed6cdc0e41a2ec4ad6fdc66f1aa4
JanSchnebi <jan.schneeberg@gmx.de>
2021-05-28 18:55:44 +0200

    Updated GiveawayListener
    nutzt nun den boolean running.
    
    Beugt Fehlern vor

--- src/de/Schnebi/Listeners/GiveawayListener.java
+++ src/de/Schnebi/Listeners/GiveawayListener.java
@@ -37,87 +37,97 @@ public class GiveawayListener extends ListenerAdapter {
     public static Timer timerNr2 = new Timer();
 
     public static String messageID;
+    public static boolean running = false;
 
     @Override
 
     public void onMessageReceived(MessageReceivedEvent event) {
+        embedBuilder.clear();
+        embedBuilder.setTitle("Giveaway by");
+        embedBuilder.setColor(Color.GREEN);
+        embedBuilder.setFooter("+blubb");
+
         String[] args = event.getMessage().getContentDisplay().split(" ");
         String messageContent = event.getMessage().getContentDisplay();
         if (args[0].equalsIgnoreCase("+giveaway")) {
-            event.getChannel().deleteMessageById(event.getMessageId()).queue();
-            try {
-                item = messageContent.substring(10, messageContent.length() - args[args.length - 1].length() - 1);
+            if (running) {
+                embedBuilder.addField("Giveaway am laufen", "Es ist bereits ein Giveaway am laufen. Bitte warte bis das Giveaway zu Ende ist", false);
+                event.getChannel().sendMessage(embedBuilder.build()).queue();
+            } else {
+                event.getChannel().deleteMessageById(event.getMessageId()).queue();
+                try {
+                    item = messageContent.substring(10, messageContent.length() - args[args.length - 1].length() - 1);
 
-                time = Integer.parseInt(args[args.length - 1].substring(0, args[args.length - 1].length() - 1));
-                unit = args[args.length - 1].substring(args[args.length - 1].length() - 1, args[args.length - 1].length());
+                    time = Integer.parseInt(args[args.length - 1].substring(0, args[args.length - 1].length() - 1));
+                    unit = args[args.length - 1].substring(args[args.length - 1].length() - 1, args[args.length - 1].length());
 
-                if (unit.equalsIgnoreCase("s")) {
-                    timeInMS = (long) time * secondMS;
-                } else if (unit.equalsIgnoreCase("m")) {
-                    timeInMS = (long) time * 60 * secondMS;
-                } else if (unit.equalsIgnoreCase("h")) {
-                    timeInMS = (long) time * 60 * 60 * secondMS;
-                } else if (unit.equalsIgnoreCase("d")) {
-                    timeInMS = (long) 24 * 60 * 60 * secondMS;
-                }
+                    if (unit.equalsIgnoreCase("s")) {
+                        timeInMS = (long) time * secondMS;
+                    } else if (unit.equalsIgnoreCase("m")) {
+                        timeInMS = (long) time * 60 * secondMS;
+                    } else if (unit.equalsIgnoreCase("h")) {
+                        timeInMS = (long) time * 60 * 60 * secondMS;
+                    } else if (unit.equalsIgnoreCase("d")) {
+                        timeInMS = (long) 24 * 60 * 60 * secondMS;
+                    }
 
-                embedBuilder.clear();
-                embedBuilder.setTitle("Giveaway by");
-                embedBuilder.setColor(Color.GREEN);
-                embedBuilder.setFooter("+blubb");
-                embedBuilder.addField(event.getMember().getEffectiveName(), "Zu gewinnen gibt es '" + item + "'. Reagiert auf diese Nachricht um als Teilnehmer registriert zu werden\n", false);
-                
-                timer = new Timer();
-                timerNr2 = new Timer();
+                    embedBuilder.addField(event.getMember().getEffectiveName(), "Zu gewinnen gibt es '" + item + "'. Reagiert auf diese Nachricht um als Teilnehmer registriert zu werden\n", false);
 
-                event.getChannel().sendMessage(embedBuilder.build()).queue(message -> {
-                    message.addReaction("U+1F389").queue();
-                    messageID = message.getId();
-                    
-                    timerNr2.scheduleAtFixedRate(new TimerTask() {
+                    timer = new Timer();
+                    timerNr2 = new Timer();
+
+                    event.getChannel().sendMessage(embedBuilder.build()).queue(message -> {
+                        message.addReaction("U+1F389").queue();
+                        messageID = message.getId();
+
+                        timerNr2.scheduleAtFixedRate(new TimerTask() {
+                            public void run() {
+                                embedBuilder.clear();
+                                embedBuilder.setFooter("+blubb");
+                                embedBuilder.setColor(Color.GREEN);
+                                embedBuilder.setTitle("Giveaway by");
+                                embedBuilder.addField(event.getMember().getEffectiveName(), "Zu gewinnen gibt es '" + item + "'. Reagiert auf diese Nachricht um als Teilnehmer registriert zu werden\n", false);
+                                embedBuilder.addField("Verbleibende Zeit", convertmillis(timeInMS), false);
+                                message.editMessage(embedBuilder.build()).queue();
+                                timeInMS = timeInMS - 5L * secondMS;
+                            }
+                        }, 0, 5L * secondMS);
+
+                        timer.schedule(new TimerTask() {
+                            public void run() {
+                                message.delete().queue();
+                                timerNr2.cancel();
+                                timerNr2.purge();
+                            }
+                        }, timeInMS);
+                    });
+
+                    running = true;
+
+                    timerReveal = new Timer();
+                    timerReveal.schedule(new TimerTask() {
                         public void run() {
+                            Random random = new Random();
+                            int randomNumer = random.nextInt(participants.size());
                             embedBuilder.clear();
+                            embedBuilder.setTitle("Giveaway by");
                             embedBuilder.setFooter("+blubb");
                             embedBuilder.setColor(Color.GREEN);
-                            embedBuilder.setTitle("Giveaway by");
-                            embedBuilder.addField(event.getMember().getEffectiveName(), "Zu gewinnen gibt es '" + item + "'. Reagiert auf diese Nachricht um als Teilnehmer registriert zu werden\n", false);
-                            embedBuilder.addField("Verbleibende Zeit", convertmillis(timeInMS), false);
-                            message.editMessage(embedBuilder.build()).queue();
-                            timeInMS = timeInMS - 5L * secondMS;
-                        }
-                    }, 0 , 5L * secondMS);
-                    
-                    timer.schedule(new TimerTask() {
-                        public void run() {
-                            message.delete().queue();
-                            timerNr2.cancel();
-                            timerNr2.purge();
+                            embedBuilder.addField(event.getMember().getEffectiveName(), "Der Gewinner des Giveaways von " + item + " ist " + participants.get(randomNumer).getAsMention()
+                                    + "!\nGlückwunsch an ihn und viel Glück beim nächsten Mahl an die anderen " + (participants.size() - 1) + " Teilnehmer!", false);
+
+                            event.getChannel().sendMessage(embedBuilder.build()).queue();
+                            running = false;
                         }
                     }, timeInMS);
-                });
-
-                timerReveal = new Timer();
-                timerReveal.schedule(new TimerTask() {
-                    public void run() {
-                        Random random = new Random();
-                        int randomNumer = random.nextInt(participants.size());
-                        embedBuilder.clear();
-                        embedBuilder.setTitle("Giveaway by");
-                        embedBuilder.setFooter("+blubb");
-                        embedBuilder.setColor(Color.GREEN);
-                        embedBuilder.addField(event.getMember().getEffectiveName(), "Der Gewinner des Giveaways von " + item + " ist " + participants.get(randomNumer).getAsMention()
-                                + "!\nGlÃ¼ckwunsch an ihn und viel GlÃ¼ck beim nÃ¤chsten Mahl an die anderen " + (participants.size() - 1) + " Teilnehmer!", false);
-
-                        event.getChannel().sendMessage(embedBuilder.build()).queue();
-                    }
-                }, timeInMS);
-            } catch (Exception e) {
-                embedBuilder.clear();
-                embedBuilder.setTitle("Blubbspinat Bot");
-                embedBuilder.setFooter("+blubb");
-                embedBuilder.setColor(Color.GREEN);
-                embedBuilder.addField("Fehler", "Falsche Verwendung des Commands.\nKorrekte Verwendung: +giveaway [item] [dauer]. \nDauer kann in Minuten (m), Stunden (h) oder Tagen (d) angegeben werden.", false);
-                event.getChannel().sendMessage(embedBuilder.build()).queue();
+                } catch (Exception e) {
+                    embedBuilder.clear();
+                    embedBuilder.setTitle("Blubbspinat Bot");
+                    embedBuilder.setFooter("+blubb");
+                    embedBuilder.setColor(Color.GREEN);
+                    embedBuilder.addField("Fehler", "Falsche Verwendung des Commands.\nKorrekte Verwendung: +giveaway [item] [dauer]. \nDauer kann in Minuten (m), Stunden (h) oder Tagen (d) angegeben werden.", false);
+                    event.getChannel().sendMessage(embedBuilder.build()).queue();
+                }
             }
         }
     }
@@ -135,7 +145,7 @@ public class GiveawayListener extends ListenerAdapter {
             System.out.println("Removed User to participants, user is " + ReactionRemoveEvent.getMember());
         }
     }
-    
+
     public static String convertmillis(long input) {
         int days = 0, hours = 0, minutes = 0, seconds = 0, millis;
 
