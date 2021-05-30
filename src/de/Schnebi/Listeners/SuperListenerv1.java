faecfabd95566f1c4d16d8040c3a71436dd6a3b1
JanSchnebi <jan.schneeberg@gmx.de>
2021-05-30 14:54:32 +0200

    Superlistener grob fertig gestellt
    Grob gertig. Alles übernommen und 140 Zeilen per Hand formatiert.
    *angry Schnebi noises
    
    Bugs nicht gefixxt, es gibt eventuell welche
    
    Leistung noch nicht optimisiert

--- src/de/Schnebi/Listeners/SuperListenerv1.java
+++ src/de/Schnebi/Listeners/SuperListenerv1.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -25,7 +26,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  */
 public class SuperListenerv1 extends ListenerAdapter {
     
-    boolean toggledSpaces = false;
+    boolean toggledSpaces = false, nonEmbed = false;
     String prefix = "+", messageContent, GifLink;
     String[] args;
     Member eventMember;
@@ -143,6 +144,15 @@ public class SuperListenerv1 extends ListenerAdapter {
                             embedBuilder.addField("Frühstück", "Also Frühstück ist wenn zwei Individuuen einer Tierart (oder unterschiedlicher Tierarten) sich zu einem nächtlichen oder täglichen Beischlaf treffen, um die Möglichkeit der Fortpflanzung herbeizuführen oder  skandalöserweise einfach Spaß zu haben", false);
                             embedBuilder.addField("~" + BETTER_member123.getEffectiveName(), "https://discord.com/channels/788794254125432849/790513198662680616/824921467002028032", false);
                             break;
+                    case "münzwürf":
+                    case "coinflip":
+                        luckHandler(event, "coin");
+                        break;
+                    case "würfel":
+                    case "dice":
+                        luckHandler(event, "dice");
+                        break;
+                        
                     default:
                         embedBuilder.addField(":thinking: hmmm", "Ups, da lief was schief. Ich kenne diesen Command nicht", false);
                 }
@@ -152,6 +162,10 @@ public class SuperListenerv1 extends ListenerAdapter {
                     case "stats":
                     case "memberstats":
                         memberStats();
+                        break;
+                    case "dice":
+                    case "würfel":
+                        luckHandler(event, "customdice");
                         break;
                     case "toggle":
                         switch (command[1]) {
@@ -173,7 +187,9 @@ public class SuperListenerv1 extends ListenerAdapter {
                     case "gifs":
                         gifHandler(event, command[1]);
                         break;
- 
+                    case "werist":
+                        weristHandler(event);
+                        break;
                 }
                 break;
                 
@@ -181,9 +197,9 @@ public class SuperListenerv1 extends ListenerAdapter {
                 embedBuilder.addField(":thinking: hmmm", "Ups, da lief was schief. Ich kenne diesen Command nicht", false);
                 
                 }
-        
+        if (!nonEmbed) {
         Channel.sendMessage(embedBuilder.build()).queue();
-        
+        }
         //Nachricht muss am ende des Switches gesendet werden. Ausnahmefälle sowie unbekannte Commands müssen mit einer Fehlermeldung ausgegeben werden
         //embedBuilder.addField(":thinking: hmmm", "Ups, da lief was schief. Ich kenne diesen Command nicht", false);
         
@@ -211,13 +227,180 @@ public class SuperListenerv1 extends ListenerAdapter {
         }
         switch (GifLink) {
             case "default":
-                Channel.sendMessage(embedBuilder.build()).queue();
                 break;
             default:
                 Channel.sendMessage(GifLink).queue();
+                nonEmbed = true;
         }
     }
         
+    void luckHandler(MessageReceivedEvent event, String type) {
+        Random random = new Random();
+        int randomNumber;
+        switch (type) {
+            case "coin":
+                String outcome;
+                
+                randomNumber = new Random().nextInt(101) - 1;
+                
+                switch (randomNumber % 50) {
+                    case 0:
+                        outcome = "Kopf";
+                        break;
+                    case 1:
+                        outcome = "Zahl";
+                        break;
+                    default:
+                        outcome = "äähhhhm, das hätte nicht passieren sollen";
+                }
+                embedBuilder.addField("Ergebnis des Münzwurfs: ", "**" + outcome + "**", false);
+                break;
+            case "dice":
+                randomNumber = random.nextInt(6) + 1;
+                embedBuilder.addField("Würfelwurf:", Integer.toString(randomNumber), false);
+                break;
+            case "customdice":
+                int randomNumer = random.nextInt(Integer.parseInt(args[1]));
+                embedBuilder.addField("Würfelwurf:", Integer.toString(randomNumer), false);
+                break;
+        }
+    }
+    
+    void weristHandler(MessageReceivedEvent event) {
+        String member = event.getMessage().getContentRaw().replaceAll("[<@!>]", "").toLowerCase();
+        switch (member) {
+            case "jan":
+            case "schnebi":
+            case "264396390199721984":
+                embedBuilder.addField("Jan Schneeberg", "Bester Developer, bescheiden, Basketballer, 2.04m groß, Lauch, versucht das Thema von <@688816780810125413> Undercover Podcasts herauszufinden.", false);
+                System.out.println("WerIstListener: Member: Jan Schneeberg");
+            break;
+            case "lea":
+            case "688816780810125413":
+                embedBuilder.addField("Lea", "Chefin, Eigentürmerin des Servers, <@&788822739556237353>, trotzdem klein und laut, CIA Agentin, hat ein Undercover Podcast, kommt aus Rohracker", false);
+                embedBuilder.addField("Hogwartshaus", "Slytherin", false);
+                System.out.println("WerIstListener: Member: Lea");
+                break;
+            case "moritz":
+            case "694873278656938034":
+                embedBuilder.addField("Moritz Breitenbach", "Saxophon", false);
+                System.out.println("WerIstListener: Member: Moritz Breitenbach");
+                break;
+            case "runa":
+            case "weber":
+            case "runa weber":
+            case "789479515495333889":
+                embedBuilder.addField("Runa Weber", "ahnungslos, 'Hobbies wie Theater tanzen singen'", false);
+                System.out.println("WerIstListener: Member: Runa Weber");
+                break;
+            case "konrad":
+            case "604265581340000273":
+                embedBuilder.addField("Konrad Olliver Oscar-Gustav", "Örtlicher IT-Support", false);
+                System.out.println("WerIstListener: Member: Konrad Harnisch-John");
+                break;
+            case "julian":
+            case "787709661842505748":
+                embedBuilder.addField("Julian", "Katzenhasser, Stock, Herr der Ringe Fanboy und leidenschaftlicher Toilettentieftaucher", false);
+                System.out.println("WerIstListener: Member: Julian");
+                break;
+            case "finn":
+            case "279268029131390977":
+                embedBuilder.addField("Finn", "Der beste und seriöseste Chefredakteur den die Welt je gesehen hat, Stupedidianer, Verfechter der Tommy-Ideologie", false);
+                System.out.println("WerIstListener: Member: Finn");
+                break;
+            case "pascal":
+            case "278972830433935371":
+                embedBuilder.addField("Unbekannter", "Ein mystischer Unbekannter welcher sich aus der Datenbank hat löschen lassen um anonym zu bleiben", false);
+                System.out.println("WerIstListener: Member: Pascal");
+                break;
+            case "pia":
+            case "413680889365528577":
+                embedBuilder.addField("Pia", "verrückter Scheinriese der Musical addicted ist", false);
+                System.out.println("WerIstListener: Member: Pia");
+                break;
+            case "magda":
+            case "787713619570982934":
+                embedBuilder.addField("Magda", " ", false);
+                System.out.println("WerIstListener: Member: Magda");
+                break;
+            case "laura":
+            case "778704706734260245":
+                embedBuilder.addField("Laura", "die verlächeltste Person auf dem Server", false);
+                System.out.println("WerIstListener: Member: Laura");
+                break;
+            case "jakob":
+            case "760144337542709318":
+                embedBuilder.addField("Jakob", "Kanisterkopf, Micheal Jordan v2, Blondie mit langen Haaren, wohnt in Kemnat, mag die Ära des Deutschen Kaiserreichs, Nachfahre Bismarcks, liebt Gollum, Gryffindor", false);
+                System.out.println("WerIstListener: Member: Jakob");
+                break;
+            case "julie":
+            case "789411377118445598":
+                embedBuilder.addField("Julie", "Kaktusliebende Gingerhexe die in ihrem Kaktuszirkel ihre Zaubertänze ausführt", false);
+                embedBuilder.addField("Hogwartshaus", "Hufflepuff", false);
+                System.out.println("WerIstListener: Member: Julie");
+                break;
+            case "lukas":
+            case "683360085178974257":
+                embedBuilder.addField("Lucas", " ", false);
+                System.out.println("WerIstListener: Member: Lucas");
+                break;
+            case "heni":
+            case "hendrick":
+            case "534078759444283412":
+                embedBuilder.addField("Hendrick 'Heni'", "Professioneller Hypixel Skyblock Tryhard", false);
+                System.out.println("WerIstListener: Member: Hendrick");
+                break;
+            case "fabian":
+            case "475405721421479957":
+                embedBuilder.addField("Fabian", " ", false);
+                System.out.println("WerIstListener: Member: Fabian");
+                break; 
+            case "hannah":
+            case "797196765647339572":
+                embedBuilder.addField("Hannah", " ", false);
+                System.out.println("WerIstListener: Member: Hannah");
+                break; 
+            case "leonie":
+            case "718449430306750495":
+                embedBuilder.addField("Leonie", " ", false);
+                System.out.println("WerIstListener: Member: Leonie");
+                break; 
+            case "franzi":
+            case "787712469429911562":
+                embedBuilder.addField("Franzi", " ", false);
+                System.out.println("WerIstListener: Member: Franzi");
+                break; 
+            case "fränz":
+            case "787717075581927465":
+                embedBuilder.addField("Fränz", " ", false);
+                System.out.println("WerIstListener: Member: Fränz");
+                break; 
+            case "lara":
+            case "694521636518494219":
+                embedBuilder.addField("Lara", " ", false);
+                System.out.println("WerIstListener: Member: Lara");
+                break; 
+            case "miri":
+            case "771473638335316050":
+                embedBuilder.addField("Miri", " ", false);
+                System.out.println("WerIstListener: Member: Miri");
+                break;
+            case "rebecca":
+            case "690590818754494565":
+                embedBuilder.addField("Rebecca", " ", false);
+                System.out.println("WerIstListener: Member: Rebecca");
+                break;
+            case "jasper":
+            case "233603431934328833":
+                embedBuilder.addField("Jasper", "Ein Pinguin der gerne mit GNUs chillt", false);
+                System.out.println("WerIstListener: Member: Jasper");
+                break;
+            default:
+                embedBuilder.addField("Unbekannter", "Ein mystischer Unbekannter welchen ich nicht in meiner Datenbank finde. Sollte es sich um einen Fehler handeln benachrichtige bitte <@264396390199721984>", false);
+                break;
+        }
+    }
+    
     void memberStats() {
         if (args.length == 1) {
             embedBuilder.addField("User: ", eventMember.getEffectiveName(), false);
