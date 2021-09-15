141f7a19a4288d8527e4a321a75d36132ce0c442
JanSchnebi <jan.schneeberg@gmx.de>
2021-09-15 21:18:40 +0200

    Final
    Finished rewriting, still got to do bugfixing

--- src/de/Schnebi/Listeners/SuperListenerV2.java
+++ src/de/Schnebi/Listeners/SuperListenerV2.java
@@ -6,9 +6,13 @@
 package de.Schnebi.Listeners;
 
 import java.awt.Color;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -110,13 +114,12 @@ public class SuperListenerV2 extends ListenerAdapter {
                         + "Da kam die Polizei, fragt 'Was ist denn das?'\nDrei Chinesen mit dem Kontrabass.";
                 embedBuilder.addField("3 Chinesen mit nem Kontrabass", orignal_text.replaceAll(command[1], command[2]), false);
             } else if (command[0].equalsIgnoreCase("werist")) {
-                
-            } else if (command.length == 3 && (command[0].equalsIgnoreCase("giveaway") || command[0].equalsIgnoreCase("umfrage"))) {
+                weristHandler();
+            } else if (command[0].equalsIgnoreCase("stats") || command[0].equalsIgnoreCase("memberstats")) {
+                memberStats();
+            }else if (command.length == 3 && (command[0].equalsIgnoreCase("giveaway") || command[0].equalsIgnoreCase("umfrage"))) {
                 send_on_final = false;
             }
-            
-            
-            
         }
         
         if (command.length == 2) {
@@ -187,6 +190,9 @@ public class SuperListenerV2 extends ListenerAdapter {
                 case "dice":
                     luckHandler(event, "dice");
                     break;
+                case "stats":
+                case "memberstats":
+                    memberStats();
             }
         }
 
@@ -197,6 +203,301 @@ public class SuperListenerV2 extends ListenerAdapter {
         Channel.deleteMessageById(message.getId()).queue();
         if (send_on_final) {
             Channel.sendMessage(embedBuilder.build()).queue();
+        }
+    }
+    
+    void weristHandler() {
+        String member = message.getContentRaw().substring(8).replaceAll("[<@!>]", "").toLowerCase();
+        switch (member) {
+            case "jan":
+            case "schnebi":
+            case "264396390199721984":
+                embedBuilder.addField("Jan Schneeberg", "Bester Developer, bescheiden, Basketballer, 2.04m groß, Lauch, versucht das Thema von <@688816780810125413> Undercover Podcasts herauszufinden.", false);
+            break;
+            case "lea":
+            case "688816780810125413":
+                embedBuilder.addField("Lea", "Chefin, Eigentürmerin des Servers, <@&788822739556237353>, trotzdem klein und laut, CIA Agentin, hat ein Undercover Podcast, kommt aus Rohracker", false);
+                embedBuilder.addField("Hogwartshaus", "Slytherin", false);
+                break;
+            case "moritz":
+            case "694873278656938034":
+                embedBuilder.addField("Moritz Breitenbach", "Saxophon", false);
+                break;
+            case "runa":
+            case "weber":
+            case "runa weber":
+            case "789479515495333889":
+                embedBuilder.addField("Runa Weber", "ahnungslos, 'Hobbies wie Theater tanzen singen'", false);
+                break;
+            case "konrad":
+            case "604265581340000273":
+                embedBuilder.addField("Konrad Olliver Oscar-Gustav", "Örtlicher IT-Support", false);
+                break;
+            case "julian":
+            case "787709661842505748":
+                embedBuilder.addField("Julian", "Katzenhasser, Stock, Herr der Ringe Fanboy und leidenschaftlicher Toilettentieftaucher", false);
+                break;
+            case "finn":
+            case "279268029131390977":
+                embedBuilder.addField("Finn", "Der beste und seriöseste Chefredakteur den die Welt je gesehen hat, Stupedidianer, Verfechter der Tommy-Ideologie", false);
+                break;
+            case "pascal":
+            case "278972830433935371":
+                embedBuilder.addField("Unbekannter", "Ein mystischer Unbekannter welcher sich aus der Datenbank hat löschen lassen um anonym zu bleiben", false);
+                break;
+            case "pia":
+            case "413680889365528577":
+                embedBuilder.addField("Pia", "verrückter Scheinriese der Musical addicted ist", false);
+                break;
+            case "magda":
+            case "787713619570982934":
+                embedBuilder.addField("Magda", " ", false);
+                break;
+            case "laura":
+            case "778704706734260245":
+                embedBuilder.addField("Laura", "die verlächeltste Person auf dem Server", false);
+                break;
+            case "jakob":
+            case "760144337542709318":
+                embedBuilder.addField("Jakob", "Kanisterkopf, Micheal Jordan v2, Blondie mit langen Haaren, wohnt in Kemnat, mag die Ära des Deutschen Kaiserreichs, Nachfahre Bismarcks, liebt Gollum, Gryffindor", false);
+                break;
+            case "julie":
+            case "789411377118445598":
+                embedBuilder.addField("Julie", "Kaktusliebende Gingerhexe die in ihrem Kaktuszirkel ihre Zaubertänze ausführt", false);
+                embedBuilder.addField("Hogwartshaus", "Hufflepuff", false);
+                break;
+            case "lukas":
+            case "683360085178974257":
+                embedBuilder.addField("Lucas", " ", false);
+                break;
+            case "heni":
+            case "hendrick":
+            case "534078759444283412":
+                embedBuilder.addField("Hendrick 'Heni'", "Professioneller Hypixel Skyblock Tryhard", false);
+                break;
+            case "fabian":
+            case "475405721421479957":
+                embedBuilder.addField("Fabian", " ", false);
+                break; 
+            case "hannah":
+            case "797196765647339572":
+                embedBuilder.addField("Hannah", " ", false);
+                break; 
+            case "leonie":
+            case "718449430306750495":
+                embedBuilder.addField("Leonie", " ", false);
+                break; 
+            case "franzi":
+            case "787712469429911562":
+                embedBuilder.addField("Franzi", " ", false);
+                break; 
+            case "fränz":
+            case "787717075581927465":
+                embedBuilder.addField("Fränz", " ", false);
+                break; 
+            case "lara":
+            case "694521636518494219":
+                embedBuilder.addField("Lara", " ", false);
+                break; 
+            case "miri":
+            case "771473638335316050":
+                embedBuilder.addField("Miri", " ", false);
+                break;
+            case "rebecca":
+            case "690590818754494565":
+                embedBuilder.addField("Rebecca", " ", false);
+                break;
+            case "jasper":
+            case "233603431934328833":
+                embedBuilder.addField("Jasper", "Ein Pinguin der gerne mit GNUs chillt", false);
+                break;
+            default:
+                embedBuilder.addField("Unbekannter", "Ein mystischer Unbekannter welchen ich nicht in meiner Datenbank finde. Sollte es sich um einen Fehler handeln benachrichtige bitte <@264396390199721984>", false);
+                break;
+        }
+    }
+    
+    void memberStats() {
+        if (args.length == 1) {
+            embedBuilder.addField("User: ", eventMember.getEffectiveName(), false);
+            embedBuilder.addField("ID", eventMember.getId(), false);
+            //
+            //Created
+            LocalDate TimeCreatedLocalDate = eventMember.getTimeCreated().toLocalDate();
+            LocalTime TimeCreatedLocalTime = (eventMember.getTimeCreated().toLocalTime());
+
+            String[] TimeArgs = TimeCreatedLocalDate.toString().split("-");
+            String TimeCreated = TimeArgs[2] + "." + TimeArgs[1] + "." + TimeArgs[0];
+
+            embedBuilder.addField("Userprofil erstellt:", TimeCreated + "\n" + TimeCreatedLocalTime.toString().substring(0, 8), false);
+
+            //
+            //Joined
+            LocalDate TimeJoinedLocalDate = eventMember.getTimeJoined().toLocalDate();
+            LocalTime TimeJoinedLocalTime = eventMember.getTimeJoined().toLocalTime();
+
+            String[] TimeArgs2 = TimeJoinedLocalDate.toString().split("-");
+            String TimeJoined = TimeArgs2[2] + "." + TimeArgs2[1] + "." + TimeArgs2[0];
+
+            embedBuilder.addField("Dem Server beigetreten:", TimeJoined + "\n" + TimeJoinedLocalTime.toString().substring(0, 8), false);
+
+            //
+            //OnGuildSince
+            //                       Ja  Fe  Ma  Ap  Ma Jun Jul  Au Sep  Ok  No  De
+            int[] monthsInDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            //declaring shit
+            LocalDate DateNow, DateJoinedGuild;
+
+            //initialising the DATE
+            DateNow = LocalDate.now();
+            DateJoinedGuild = eventMember.getTimeJoined().toLocalDate();
+
+            //getting Day, Month and Year
+            int DateNowDay = DateNow.getDayOfMonth(), DateNowMonth = DateNow.getMonthValue(), DateNowYear = DateNow.getYear();
+            int DateJoinedGuildDay = DateJoinedGuild.getDayOfMonth(), DateJoinedGuildMonth = DateJoinedGuild.getMonthValue(), DateJoinedGuildYear = DateJoinedGuild.getYear();
+
+            int DateOnServerYear, DateOnServerMonth, DateOnServerDay;
+
+            //Calculating Time on Guild in Date
+            DateOnServerDay = DateNowDay - DateJoinedGuildDay;
+            DateOnServerMonth = DateNowMonth - DateJoinedGuildMonth;
+            DateOnServerYear = DateNowYear - DateJoinedGuildYear;
+
+            if (DateOnServerDay < 0) {
+                DateOnServerDay = DateOnServerDay + monthsInDays[DateJoinedGuildMonth];
+                DateOnServerMonth = DateOnServerMonth - 1;
+            }
+            if (DateOnServerMonth < 0) {
+                DateOnServerMonth = DateOnServerMonth + 12;
+                DateOnServerYear = DateOnServerYear - 1;
+            }
+
+            int JahrNow = DateNowYear, JahrPast = DateJoinedGuildYear;
+            int[] Jahre = (IntStream.rangeClosed(JahrPast, JahrNow).toArray());
+
+            for (int j : Jahre) {
+                if (j % 4 == 0) {
+                    DateOnServerDay++;
+                }
+            }
+            String DateOnServer = "";
+
+            String strYear = " Jahren", strMonth = " Monaten und ", strDay = " Tagen";
+            if (DateOnServerYear == 1) {
+                strYear = " Jahr";
+            }
+            if (DateOnServerMonth == 1) {
+                strMonth = " Monat und ";
+            }
+            if (DateOnServerDay == 1) {
+                strDay = " Tag";
+            }
+
+            if (DateOnServerYear > 0) {
+                DateOnServer = DateOnServerYear + strYear + DateOnServerMonth + strMonth + DateOnServerDay + strDay;
+            } else if (DateOnServerMonth > 0) {
+                DateOnServer = DateOnServerMonth + strMonth + DateOnServerDay + strDay;
+            } else if (DateOnServerDay > 0) {
+                DateOnServer = DateOnServerDay + strDay;
+            }
+
+            embedBuilder.addField("Auf dem Server seit:", DateOnServer, true);
+            //Message send
+            System.out.println("CommandListener: Detected message 'stats' with argslength of 1. Responding by to User " + eventMember.getEffectiveName() + " by giving following stats: \n" + TimeCreated + "\n"
+                    + TimeCreatedLocalTime.toString().substring(0, 8) + "\n" + TimeJoined + "\n" + TimeJoinedLocalTime.toString().substring(0, 8) + "\nmay be not correct idk didnt rework this");
+        } else {
+            List<Member> TaggedUsers = message.getMentionedMembers();
+            Member MemberStats = TaggedUsers.get(0);
+
+            if (MemberStats.hasPermission(Permission.VIEW_CHANNEL)) {
+
+                embedBuilder.addField("User: ", MemberStats.getEffectiveName(), false);
+                embedBuilder.addField("ID", MemberStats.getId(), false);
+                //                        
+                //Created
+                LocalDate TimeCreatedLocalDate = MemberStats.getTimeCreated().toLocalDate();
+                LocalTime TimeCreatedLocalTime = MemberStats.getTimeCreated().toLocalTime();
+
+                String[] TimeArgs = TimeCreatedLocalDate.toString().split("-");
+                String TimeCreated = TimeArgs[2] + "." + TimeArgs[1] + "." + TimeArgs[0];
+
+                embedBuilder.addField("Userprofil erstellt:", TimeCreated + "\n" + TimeCreatedLocalTime.toString().substring(0, 8), false);
+
+                //
+                //Joined
+                LocalDate TimeJoinedLocalDate = MemberStats.getTimeJoined().toLocalDate();
+                LocalTime TimeJoinedLocalTime = MemberStats.getTimeJoined().toLocalTime();
+
+                String[] TimeArgs2 = TimeJoinedLocalDate.toString().split("-");
+                String TimeJoined = TimeArgs2[2] + "." + TimeArgs2[1] + "." + TimeArgs2[0];
+
+                embedBuilder.addField("Dem Server beigetreten:", TimeJoined + "\n" + TimeJoinedLocalTime.toString().substring(0, 8), false);
+
+                //
+                //OnGuildSince
+                //                       Ja  Fe  Ma  Ap  Ma Jun Jul  Au Sep  Ok  No  De
+                int[] monthsInDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+                //declaring shit
+                LocalDate DateNow, DateJoinedGuild;
+
+                //initialising the DATE
+                DateNow = LocalDate.now();
+                DateJoinedGuild = MemberStats.getTimeJoined().toLocalDate();
+
+                //getting Day, Month and Year
+                int DateNowDay = DateNow.getDayOfMonth(), DateNowMonth = DateNow.getMonthValue(), DateNowYear = DateNow.getYear();
+                int DateJoinedGuildDay = DateJoinedGuild.getDayOfMonth(), DateJoinedGuildMonth = DateJoinedGuild.getMonthValue(), DateJoinedGuildYear = DateJoinedGuild.getYear();
+
+                int DateOnServerYear = 0, DateOnServerMonth = 0, DateOnServerDay = 0;
+
+                //Calculating Time on Guild in Date 
+                DateOnServerDay = DateNowDay - DateJoinedGuildDay;
+                DateOnServerMonth = DateNowMonth - DateJoinedGuildMonth;
+                DateOnServerYear = DateNowYear - DateJoinedGuildYear;
+
+                if (DateOnServerDay < 0) {
+                    DateOnServerDay = DateOnServerDay + monthsInDays[DateJoinedGuildMonth];
+                    DateOnServerMonth = DateOnServerMonth - 1;
+                }
+                if (DateOnServerMonth < 0) {
+                    DateOnServerMonth = DateOnServerMonth + 12;
+                    DateOnServerYear = DateOnServerYear - 1;
+                }
+
+                int JahrNow = DateNowYear, JahrPast = DateJoinedGuildYear;
+                int[] Jahre = (IntStream.rangeClosed(JahrPast, JahrNow).toArray());
+
+                for (int j : Jahre) {
+                    if (j % 4 == 0) {
+                        DateOnServerDay++;
+                    }
+                }
+
+                String DateOnServer = "";
+
+                String strYear = " Jahren", strMonth = " Monaten und ", strDay = " Tagen";
+                if (DateOnServerYear == 1) {
+                    strYear = " Jahr";
+                }
+                if (DateOnServerMonth == 1) {
+                    strMonth = " Monat und ";
+                }
+                if (DateOnServerDay == 1) {
+                    strDay = " Tag";
+                }
+
+                if (DateOnServerYear > 0) {
+                    DateOnServer = DateOnServerYear + strYear + DateOnServerMonth + strMonth + DateOnServerDay + strDay;
+                } else if (DateOnServerMonth > 0) {
+                    DateOnServer = DateOnServerMonth + strMonth + DateOnServerDay + strDay;
+                } else if (DateOnServerDay > 0) {
+                    DateOnServer = DateOnServerDay + strDay;
+                }
+
+                embedBuilder.addField("Auf dem Server seit:", DateOnServer, true);
+            }
         }
     }
     
